e69adf7c2211d95ab70d100270d30d889d9fb60d
Jakub Solecki <solko12.98@gmail.com>
2021-04-14 18:16:17 +0200

    Dodany .gitignore Dodane rysowanie wykresu zadanie 2.1

--- Zad2_1.py
+++ Zad2_1.py
@@ -1,19 +1,35 @@
+import time
 from docplex.mp.model import Model
 import numpy as np
 import generator as gen
-n = 10
-m = Model(name='test')
+from matplotlib import pyplot as plt
 
-generator = gen.RandomNumberGenerator(578)
+size = 40
+solutions = np.zeros((size))
 
-S=[]
-for i in range(0, n):
-        S.append(generator.nextInt(-100,100))
-T = generator.nextInt(-50*n,50*n);
+for n in range(1, size, 1):
 
-y = [ m.binary_var(name='y{0}'.format(i)) for i in range(0,n) ]
+    generator = gen.RandomNumberGenerator(654)
 
-m.minimize(m.abs(T-m.sum(y[i]*S[i] for i in range(0,n))))
+    m = Model(name='test{0}'.format(n))
+    m.float_precision = 4
+    S = []
+    for i in range(0, n):
+        S.append(generator.nextInt(-100, 100))
+    T = generator.nextInt(-50*n, 50*n)
 
-m.solve()
-m.print_solution()
+    y = [m.binary_var(name='y{0}'.format(i)) for i in range(0, n, 1)]
+
+    m.minimize(m.abs(T-m.sum(y[i]*S[i] for i in range(0, n))))
+
+    # m.print_solution()
+    solutions[n] = m.solve().objective_value
+
+
+x = np.arange(0, size)
+print(solutions)
+plt.title("Wynik minimalizacji w zależności od liczby zmiennych")
+plt.xlabel("Liczba zmiennych")
+plt.ylabel("Wynik minimalizacji")
+plt.plot(x, solutions, "o")
+plt.show()
