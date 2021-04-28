5bb2a6d36defd52e4e839a3fc271bf1f918eb939
Jakub Solecki <solko12.98@gmail.com>
2021-04-28 16:26:01 +0200

    Cud, działa, a nparray jest suabe

--- Drzewiaste/Zad2_5.py
+++ Drzewiaste/Zad2_5.py
@@ -8,7 +8,7 @@ Z = 4124
 generator = gen.RandomNumberGenerator(Z)
 
 X = []
-PoczatkoweWybrane = np.zeros(n, dtype=bool)
+PoczatkoweWybrane = []
 
 C = np.zeros(n)
 W = np.zeros(n)
@@ -16,35 +16,53 @@ B = generator.nextInt(5*n, 10*n)
 for i in range(0, n):
     C[i] = generator.nextInt(1, 30)
     W[i] = generator.nextInt(1, 30)
-    PoczatkoweWybrane[i] = False
+    PoczatkoweWybrane.append(False)
 
 # BRUTE FORCE
 
 
-def szukajRozwiazania(X, PWybrane, wybor, poziom, biezacyNajlepszy):
+def szukajRozwiazania(X, PWybrane, wybor, poziom, biezacyNajlepszy, limitGlebokosci=0):
 
     PWybrane[wybor] = True
     X.append(wybor)
     sumaWag = sumujWagi(X)
-    print("Poziom: ", poziom, " Wybór: ", wybor,  " Podzbior: ", X,
-          " PWybrane: ", PWybrane, " Suma wag: ", sumaWag)
+
     if(sumaWag == B):
-        print(sumujWartość(X))
+        print("Suma wag rowna wadze ograniczajacej z suma wartosci: ", sumujWartość(X))
         return sumujWartość(X)
     if(sumaWag > B):
         #PWybrane[wybor] = False
-        #X.remove(wybor)
+        # X.remove(wybor)
         return biezacyNajlepszy
     if(poziom == n):
         return sumujWartość(X)
     biezacyNajlepszy = sumujWartość(X)
-    for i in range(0, n):
-        if(PWybrane[i] == False):
+    print("Poziom: ", poziom, " Wybór: ", wybor,  " Podzbior: ", X,
+          " PWybrane: ", PWybrane, " Suma wag: ", sumaWag, " Bieżący najlepszy: ", biezacyNajlepszy)
+    for k in range(0, n):
+        if(PWybrane[k] == False):
             nowe = szukajRozwiazania(
-                X[:], PWybrane[:], i, poziom+1, biezacyNajlepszy)
+                X[:], PWybrane[:], k, poziom+1, biezacyNajlepszy)
             if(biezacyNajlepszy < nowe):
                 biezacyNajlepszy = nowe
     return biezacyNajlepszy
+
+
+X = []
+PStart = []
+for i in range(0, n):
+    PStart.append(False)
+
+
+def szukajRozwiazaniaTest(X, P, wybor, poziom):
+    P[wybor] = True
+    X.append(wybor)
+    print("P: ", P, " Wybor: ", wybor, " X: ", X)
+    for item in range(0, n):
+        #print(poziom, ".", item, " P: ", P)
+        if(P[item] == False):
+            szukajRozwiazaniaTest(X[:], P[:], item, poziom+1)
+            P[item] = True
 
 
 def sumujWagi(X):
@@ -71,9 +89,9 @@ najlepszy = 0
 for i in range(0, n):
     X = []
     #PoczatkoweWybrane[i] = False
-    for j in range(i, n):
+    for j in range(0, n):
         PoczatkoweWybrane[j] = False
-    nowy = szukajRozwiazania(X, PoczatkoweWybrane, i, 0, 0)
+    nowy = szukajRozwiazania(X[:], PoczatkoweWybrane[:], i, 0, 0)
     if(nowy > najlepszy):
         najlepszy = nowy
 
