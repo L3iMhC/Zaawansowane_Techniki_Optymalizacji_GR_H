375d7e2891c95ea29fca6a71910d351acd7d7f0a
Jakub Solecki <solko12.98@gmail.com>
2021-04-28 23:34:13 +0200

    No nie działa :( Ale coś jest

--- Drzewiaste/Zad2_5.py
+++ Drzewiaste/Zad2_5.py
@@ -4,7 +4,7 @@ import random
 
 moduleName = '../CPLEX/generator'
 
-n = 25
+n = 5
 Z = 4124
 generator = gen.RandomNumberGenerator(Z)
 
@@ -188,12 +188,45 @@ def LiczProblemPlecakowy(Ograniczenie, Wagi, Wartosci, Poziom):
 
 # Driver Code
 
-print(LiczProblemPlecakowy(B, W, C, n))
+#print(LiczProblemPlecakowy(B, W, C, n))
 
 # This code is contributed by Nikhil Kumar Singh
 
 
-# Podejscie Greediego
+# Podejscie Greediego w sortowaniu kosztow
+
+# A simple implementation of Priority Queue
+# using Queue.
+class PriorityQueue(object):
+    def __init__(self):
+        self.queue = []
+
+    def __str__(self):
+        return ' '.join([str(i) for i in self.queue])
+
+    # for checking if the queue is empty
+    def isEmpty(self):
+        return len(self.queue) == 0
+
+    # for inserting an element in the queue
+    def insert(self, data):
+        self.queue.append(data)
+
+    # for popping an element based on Priority
+    def delete(self):
+        try:
+            max = 0
+            for i in range(len(self.queue)):
+                if self.queue[i] > self.queue[max]:
+                    max = i
+            item = self.queue[max]
+            del self.queue[max]
+            return item
+        except IndexError:
+            print()
+            exit()
+
+
 class itemWiC:
     def __init__(self, waga, wartosc, id):
         self.waga = waga
@@ -207,16 +240,126 @@ class itemWiC:
     def __lt__(self, other):
         return self.koszt < other.koszt
 
+    def __str__(self):
+        return ("Waga: ", self.waga, " Wartosc: ", self.wartosc, " ID: ", self.id)
 
-def znajdzMaksimumPlecaka(Wartosci, Wagi, MaksymalnaPojemnoscPlecaka):
+    def __repr__(self):
+        return ("\nWaga: " + str(self.waga) + " Wartosc: " + str(self.wartosc) + " ID: "+str(self.id))
+
+
+class galaz:
+    def __init__(self, poziom, zyskDoTejGalezi, UB, LB, Wybrany, wagaDoTejGalezi):
+        self.poziom = poziom
+        self.zyskDoTejGalezi = zyskDoTejGalezi
+        self.UB = UB
+        self.LB = LB
+        self.wagaDoTejGalezi = wagaDoTejGalezi
+        self.wybrany = Wybrany
+
+    def __lt__(self, other):
+        return self.LB > other.LB
+
+
+def znajdzGorneOgraniczenie(zyskDoTejGalezi, wagaDoTejGalezi, id, itemsWiC):
+    wartosc = zyskDoTejGalezi
+    waga = wagaDoTejGalezi
+    for i in range(id, n):
+        if waga+itemsWiC[i].waga <= B:
+            waga = waga + itemsWiC[i].waga
+            wartosc = wartosc - itemsWiC[i].wartosc
+        else:
+            wartosc = wartosc - (B-waga)/itemsWiC[i].waga*itemsWiC[i].wartosc
+            break
+    return wartosc
+
+
+def znajdzDolneOgraniczenie(zyskDoTejGalezi, wagaDoTejGalezi, id, itemsWiC):
+    wartosc = zyskDoTejGalezi
+    waga = wagaDoTejGalezi
+    for i in range(id, n):
+        if waga+itemsWiC[i].waga <= B:
+            waga += itemsWiC[i].waga
+            wartosc -= itemsWiC[i].wartosc
+        else:
+            break
+    return wartosc
+
+
+def przypiszGaleziWartosci(Galaz, UB, LB, poziom, Wybrany, zyskDoTejGalezi, wagaDoTejGalezi):
+    Galaz.poziom = poziom
+    Galaz.zyskDoTejGalezi = zyskDoTejGalezi
+    Galaz.UB = UB
+    Galaz.LB = LB
+    Galaz.wagaDoTejGalezi = wagaDoTejGalezi
+    Galaz.wybrany = Wybrany
+
+
+def znajdzMaksimumPlecaka(Wartosci, Wagi):
     itemWiCs = []
+    minLB = 0
+    # wartość lowerbound na lisciach w drzewie
+    ostateczneLB = 9999999999999999999999999
+    biezacaSciezka = []
+    ostatecznaSciezka = []
+    biezacaGalaz = lewaGalaz = prawaGalaz = galaz(0, 0, 0, 0, False, 0)
     for i in range(0, n):
         itemWiCs.append(itemWiC(Wagi[i], Wartosci[i], i))
-
+        ostatecznaSciezka.append(False)
+        biezacaSciezka.append(False)
+    print(itemWiCs)
     itemWiCs.sort(reverse=True)
-    maxZysk = 0
-    Q = []
-    Q.append(itemWiC(0, 0, n))
+
+    Q = PriorityQueue()
+    Q.insert(biezacaGalaz)
+    z = 0
+    while not Q.isEmpty():
+        z += 1
+
+        biezacaGalaz = Q.delete()
+        print(biezacaGalaz.UB, minLB)
+        if(biezacaGalaz.UB > minLB or biezacaGalaz.UB >= ostateczneLB):
+            continue
+
+        if(biezacaGalaz.poziom != 0):
+            biezacaSciezka[biezacaGalaz.poziom - 1] = biezacaGalaz.wybrany
+
+        if(biezacaGalaz.poziom == n-1):
+            if(biezacaGalaz.LB < ostateczneLB):
+                for i in range(0, n):
+                    ostatecznaSciezka[itemWiCs[i].id] = biezacaSciezka[i]
+            print(ostateczneLB)
+            ostateczneLB = min(biezacaGalaz.LB, ostateczneLB)
+            continue
+
+        poziom = biezacaGalaz.poziom
+
+        przypiszGaleziWartosci(prawaGalaz,
+                               znajdzGorneOgraniczenie(
+                                   biezacaGalaz.zyskDoTejGalezi, biezacaGalaz.wagaDoTejGalezi, poziom+1, itemWiCs),
+                               znajdzDolneOgraniczenie(
+                                   biezacaGalaz.zyskDoTejGalezi, biezacaGalaz.wagaDoTejGalezi, poziom+1, itemWiCs),
+                               poziom+1, False, biezacaGalaz.zyskDoTejGalezi, biezacaGalaz.wagaDoTejGalezi)
+
+        if(biezacaGalaz.wagaDoTejGalezi + itemWiCs[biezacaGalaz.poziom].waga <= B):
+            lewaGalaz.UB = znajdzGorneOgraniczenie(
+                biezacaGalaz.zyskDoTejGalezi - itemWiCs[poziom].wartosc, biezacaGalaz.wagaDoTejGalezi + itemWiCs[poziom].waga, poziom+1, itemWiCs)
+            lewaGalaz.LB = znajdzDolneOgraniczenie(
+                biezacaGalaz.zyskDoTejGalezi - itemWiCs[poziom].wartosc, biezacaGalaz.wagaDoTejGalezi + itemWiCs[poziom].waga, poziom+1, itemWiCs)
+            przypiszGaleziWartosci(lewaGalaz, lewaGalaz.UB, lewaGalaz.LB, poziom+1, True, biezacaGalaz.zyskDoTejGalezi -
+                                   itemWiCs[poziom].wartosc, biezacaGalaz.wagaDoTejGalezi + itemWiCs[poziom].waga)
+
+        else:
+            lewaGalaz.UB = lewaGalaz.LB = 1
+
+        minLB = min(minLB, lewaGalaz.LB)
+        minLB = min(minLB, prawaGalaz.LB)
+        if(minLB >= lewaGalaz.UB):
+            Q.insert(lewaGalaz)
+        if(minLB >= prawaGalaz.UB):
+            Q.insert(prawaGalaz)
+
+    print("Znaleziono maksymalna wartosc: ", ostateczneLB,
+          " Dla wyborow: ", ostatecznaSciezka)
 
 
-znajdzMaksimumPlecaka(C, W, B)
+znajdzMaksimumPlecaka(C, W)
