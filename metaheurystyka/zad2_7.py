2e56f76b486ff2c9378b762a9a7dc3f9bafb6c43
Jakub Solecki <solko12.98@gmail.com>
2021-05-06 17:50:32 +0200

    Ehh

--- metaheurystyka/zad2_7.py
+++ metaheurystyka/zad2_7.py
@@ -11,21 +11,55 @@ Z = 4124
 generator = gen.RandomNumberGenerator(Z)
 
 
-p = np.zeros(n, dtype='int32') #czas wykonania
-w = np.zeros(n, dtype='int32') #waga
-d = np.zeros(n, dtype='int32') #wymagany czas zakonczenia
-S=0
+p = np.zeros(n, dtype='int32')  # czas wykonania
+w = np.zeros(n, dtype='int32')  # waga
+d = np.zeros(n, dtype='int32')  # wymagany czas zakonczenia
+S = 0
 InitialQueue = np.zeros(n, dtype='int32')
+
+
+def RandomSearch(path):
+
+    temp = path
+    BestDistance = WeightedLatency(path)
+    iterations = 0
+
+    print('\n\n\n\nInitial Path: ', path,
+          '\nInitial Distance: ', WeightedLatency(path), '\n',)
+
+    while(iterations < 1000):
+
+        i1 = random.randint(0, n-1)
+        i2 = random.randint(0, n-1)
+        while(i1 == i2):
+            i2 = random.randint(0, n-1)
+
+        temp[i1], temp[i2] = temp[i2], temp[i1]
+        CurrentDistance = WeightedLatency(temp)
+
+        if(CurrentDistance < BestDistance):
+            iterations = 0
+            BestDistance = CurrentDistance
+            BestPath = temp.copy()
+            print('\nCurrent Best Path: ', BestPath,
+                  '\nCurrent Best Distance: ', BestDistance, '\n',)
+        else:
+            temp[i1], temp[i2] = temp[i2], temp[i1]
+            iterations = iterations + 1
+
+    return BestPath, BestDistance
+
 
 for i in range(0, n):
     InitialQueue[i] = i
     p[i] = generator.nextInt(1, 30)
     w[i] = generator.nextInt(1, 30)
-    S+=p[i]
+    S += p[i]
 for i in range(0, n):
     d[i] = generator.nextInt(1, S)
 
-np.random.shuffle(InitialQueue) #losowe rozwiazanie poczatkowe
+np.random.shuffle(InitialQueue)  # losowe rozwiazanie poczatkowe
+
 
 def WeightedLatency(queue):
     C = np.zeros(n, dtype='int32')
@@ -35,9 +69,15 @@ def WeightedLatency(queue):
         C[queue[i]] = C[queue[i-1]]+p[queue[i]]
     for i in range(0, n):
         T[queue[i]] = max(0, C[queue[i]] - d[queue[i]])
-    return w*T
+    return (w*T).sum()
 
 
 InitialLatency = WeightedLatency(InitialQueue.copy())
 
-print('Initial Path: ',InitialQueue,'\nInitial Distance: ',InitialLatency)
+print('Initial Path: ', InitialQueue, '\nInitial Distance: ', InitialLatency)
+
+RandomSearchBestQueue, RandomSearchBestValue = RandomSearch(
+    InitialQueue.copy())
+
+print('\n\n\n\n(RS)Final queue: ', RandomSearchBestQueue,
+      '\n(RS)Final Value: ', RandomSearchBestValue)
