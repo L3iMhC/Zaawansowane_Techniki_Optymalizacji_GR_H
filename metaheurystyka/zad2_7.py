baa4b1ad4d49ced1074234ad8b8e0a12152b6795
Szymon Chmielewski <szymon.chmielewski98@gmail.com>
2021-05-06 13:48:05 +0200

    initial

--- metaheurystyka/zad2_7.py
+++ metaheurystyka/zad2_7.py
@@ -15,12 +15,29 @@ p = np.zeros(n, dtype='int32') #czas wykonania
 w = np.zeros(n, dtype='int32') #waga
 d = np.zeros(n, dtype='int32') #wymagany czas zakonczenia
 S=0
-#InitialQueue = np.zeros(n, dtype='int32')
+InitialQueue = np.zeros(n, dtype='int32')
 
 for i in range(0, n):
-    #InitialQueue[i] = i
+    InitialQueue[i] = i
     p[i] = generator.nextInt(1, 30)
     w[i] = generator.nextInt(1, 30)
     S+=p[i]
 for i in range(0, n):
     d[i] = generator.nextInt(1, S)
+
+np.random.shuffle(InitialQueue) #losowe rozwiazanie poczatkowe
+
+def WeightedLatency(queue):
+    C = np.zeros(n, dtype='int32')
+    T = np.zeros(n, dtype='int32')
+    C[queue[0]] = p[queue[0]]
+    for i in range(1, n):
+        C[queue[i]] = C[queue[i-1]]+p[queue[i]]
+    for i in range(0, n):
+        T[queue[i]] = max(0, C[queue[i]] - d[queue[i]])
+    return w*T
+
+
+InitialLatency = WeightedLatency(InitialQueue.copy())
+
+print('Initial Path: ',InitialQueue,'\nInitial Distance: ',InitialLatency)
