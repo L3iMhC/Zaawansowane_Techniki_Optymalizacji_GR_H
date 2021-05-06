56ae6f808d8ab3231ebd6f533ebb42dea047feab
Szymon Chmielewski <szymon.chmielewski98@gmail.com>
2021-05-06 13:28:26 +0200

    Generator dla 2_7

---
+++ metaheurystyka/zad2_7.py
@@ -0,0 +1,26 @@
+import generator as gen
+import numpy as np
+import random
+import math
+
+moduleName = '../CPLEX/generator'
+
+
+n = 5
+Z = 4124
+generator = gen.RandomNumberGenerator(Z)
+
+
+p = np.zeros(n, dtype='int32') #czas wykonania
+w = np.zeros(n, dtype='int32') #waga
+d = np.zeros(n, dtype='int32') #wymagany czas zakonczenia
+S=0
+#InitialQueue = np.zeros(n, dtype='int32')
+
+for i in range(0, n):
+    #InitialQueue[i] = i
+    p[i] = generator.nextInt(1, 30)
+    w[i] = generator.nextInt(1, 30)
+    S+=p[i]
+for i in range(0, n):
+    d[i] = generator.nextInt(1, S)
