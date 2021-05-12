6ab64bf01d6aa77aa4f45d99e396ba79d110f284
solko12 <solko12.98@gmail.com>
2021-05-12 23:20:52 +0200

    Poczatek sasiedztwa

--- metaheurystyka/zad2_7.py
+++ metaheurystyka/zad2_7.py
@@ -24,6 +24,7 @@ InitialQueue = np.zeros(n, dtype='int32')
 def RandomSearch(queue, printMidPointValues=False):
 
     temp = queue
+    BestQueue = queue
     BestLatency = WeightedLatency(queue)
     iterations = 0
     if(printMidPointValues):
@@ -56,31 +57,50 @@ def RandomSearch(queue, printMidPointValues=False):
 
 def acceptance(x1, x2, t):
     different = -(x1-x2)
-    value = math.exp(different/t)
+    divided = different//t
+    value = math.exp(divided)
     return value
 
 
-def SimulatedAnnealing(queue, initialTemperature=1000, printMidPointValues=False):
+def SimulatedAnnealing(n, queue, initialTemperature=1000, printMidPointValues=False):
 
     temp = queue
     BestLatency = WeightedLatency(queue)
     iterations = 0
-
+    BestQueue = queue
     tmp = initialTemperature
-    finalTemp = initialTemperature/100
-    a = 0.995
+    if(tmp < 100):
+        finalTemp = 1
+    else:
+        finalTemp = initialTemperature/1000  # Final temp do dopasowania
+    a = 0.95
     if(printMidPointValues):
         print('\n\n\n\nInitial queue: ', queue,
               '\nInitial value: ', WeightedLatency(queue), '\n',)
     startTime = time.time()
-    while(iterations < 10000 or tmp > finalTemp):
-        if(time.time()-startTime > 10):
-            break
-        i1 = random.randint(0, n-1)
-        i2 = random.randint(0, n-1)
-        while(i1 == i2):
-            i2 = random.randint(0, n-1)
 
+    i1 = 0
+    i2 = n-1
+    pr = n//5  # Promien w jakim szukamy sasiadow
+    while(iterations < 10000):
+        if(time.time()-startTime > 10 or tmp < 1/finalTemp or tmp < finalTemp):  # To trzba też dostosować
+            break
+        zi1 = i1  # Srodek zakresu w jakim szukamy i1
+        zi2 = i2  # Srodek zakresu w jakim szukamy i2
+
+        if(zi1-pr < 0):
+            zi1 = zi1+pr
+        if(zi1+pr > n-1):
+            zi1 = zi1-pr
+        if(zi2-pr < 0):
+            zi2 = zi2+pr
+        if(zi2+pr > n-1):
+            zi2 = zi2-pr
+        #print(i1, i2)
+        i1 = random.randint(zi1-pr, zi1+pr)
+        i2 = random.randint(zi2-pr, zi2+pr)
+        while(i1 == i2):
+            i2 = random.randint(zi2-pr, zi2+pr)
         PreviousValue = WeightedLatency(temp)
         temp[i1], temp[i2] = temp[i2], temp[i1]
         CurrentValue = WeightedLatency(temp)
@@ -96,8 +116,7 @@ def SimulatedAnnealing(queue, initialTemperature=1000, printMidPointValues=False
             iterations = 0
         else:
             temp[i1], temp[i2] = temp[i2], temp[i1]
-
-        iterations = iterations + 1
+            iterations = iterations + 1
 
         tmp = a*tmp
 
@@ -132,8 +151,8 @@ np.random.shuffle(InitialQueue)  # losowe rozwiazanie poczatkowe
 
 
 def WeightedLatency(queue):
-    C = np.zeros(n, dtype='int32')
-    T = np.zeros(n, dtype='int32')
+    C = np.zeros(n, dtype='long')
+    T = np.zeros(n, dtype='long')
     C[queue[0]] = p[queue[0]]
     for i in range(1, n):
         C[queue[i]] = C[queue[i-1]]+p[queue[i]]
@@ -142,14 +161,15 @@ def WeightedLatency(queue):
     return (w*T).sum()
 
 
-wielkoscInstancji = 15
-
+MaxWielkoscInstancji = 50
+MinWielkoscInstancji = 10
 RSValues = []
 RSTimes = []
 SAValues = []
 SATimes = []
 InitialValues = []
-for n in range(3, wielkoscInstancji):
+for n in range(MinWielkoscInstancji, MaxWielkoscInstancji):
+    print(n)
     p = np.zeros(n, dtype='int32')  # czas wykonania
     w = np.zeros(n, dtype='int32')  # waga
     d = np.zeros(n, dtype='int32')  # wymagany czas zakonczenia
@@ -184,16 +204,16 @@ for n in range(3, wielkoscInstancji):
         initTemp = 1000
 
     startTime = time.time()
-    SimulatedAnnealingBestQueue, SimulatedAnnealingBestLatency = SimulatedAnnealing(
-        queue=InitialQueue.copy(), initialTemperature=initTemp)
+    SimulatedAnnealingBestQueue, SimulatedAnnealingBestLatency = SimulatedAnnealing(n,
+                                                                                    queue=InitialQueue.copy(), initialTemperature=initTemp)
     endTime = time.time()
     SATimes.append(endTime-startTime)
     SAValues.append(SimulatedAnnealingBestLatency)
     # print('\n\n\n\n(SA)Final queue: ', SimulatedAnnealingBestQueue,
     #       '\n(SA)Final latency: ', SimulatedAnnealingBestLatency)
 
-x = np.arange(3, wielkoscInstancji)
-plt.title("Czas maksymalizacji problemu witi na jednej maszynie w zależności od wielkości instancji")
+x = np.arange(MinWielkoscInstancji, MaxWielkoscInstancji)
+plt.title("Czas minimalizacji problemu witi na jednej maszynie w zależności od wielkości instancji")
 plt.xlabel("Liczba zmiennych")
 plt.ylabel("Czas rozwiązywania")
 plt.plot(x, RSTimes, "o", label="Random Search")
