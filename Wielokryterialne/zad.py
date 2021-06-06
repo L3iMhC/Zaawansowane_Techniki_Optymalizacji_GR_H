3e223552d60eb6526e372a74b061bb44cf3ac26c
solko12 <solko12.98@gmail.com>
2021-06-06 21:08:53 +0200

    Dodano ala wykres frontu pareto

--- Wielokryterialne/zad.py
+++ Wielokryterialne/zad.py
@@ -39,38 +39,105 @@ def all_task_done_time(schedule, n, m, p, d, ktore_kryteria_zwrocic=[2, 3, 4, 5]
         # Maksymalna nieterminowość zadania
         L_max = max(L_max, C[schedule[i]][2] - d[j])
     if(ktore_kryteria_zwrocic == [2, 3]):
-        return F, T_max
+        return [F, T_max]
     if(ktore_kryteria_zwrocic == [2, 3, 4]):
-        return F, T_max, T_sum
+        return [F, T_max, T_sum]
     if(ktore_kryteria_zwrocic == [2, 3, 4, 5]):
-        return F, T_max, T_sum, L_max
+        return [F, T_max, T_sum, L_max]
     return C[schedule[-1]][-1]
 
+# SZABLON
+# def do_zadanie1(min_tasks=4, max_tasks=10):
+#     m = 3
+#     for n in range(min_tasks, max_tasks+1):
+#         A = 0
+#         # Generacja instancji
+#         p = np.zeros((n, m))
+#         d = np.zeros((n))
+#         initial_schedule = np.zeros(n, dtype=np.int32)
+#         for i in range(0, n):
+#             initial_schedule[i] = i
+#             for j in range(0, m):
+#                 p[i][j] = generator.nextInt(1, 99)
+#                 A += p[i][j]
+#         B = A//2
+#         A = A//6
+#         for i in range(0, n):
+#             d[j] = generator.nextInt(A, B)
+#         # Losowanie początkowego harmonogramu
+#         # np.random.shuffle(initial_schedule)
+#         print(all_task_done_time(
+#             initial_schedule, n, m, p, d, [2, 3, 4, 5]))
+#         print(p)
+#         print(initial_schedule)
 
-def do_experiments(repeats=1, min_tasks=4, max_tasks=10):
+# Zadanie 1
+
+
+def is_dominated(k_x_prim, k_x):
+    betters = 0
+    for i in range(len(k_x)):
+        if k_x_prim[i] > k_x[i]:
+            return False
+        if k_x_prim[i] < k_x[i]:
+            betters += 1
+    if(betters > 0):
+        return True
+    else:
+        return False
+
+
+def make_Pareto_from_P(P):
+    F = P.copy()
+    for a in F:
+        for b in F:
+            if(a != b and is_dominated(b, a)):
+                F.remove(a)
+                break
+    return F
+
+
+def do_zadanie1(min_tasks=4, max_tasks=10):
     m = 3
     for n in range(min_tasks, max_tasks+1):
-        for _ in range(0, repeats):
-            A = 0
-            # Generacja instancji
-            p = np.zeros((n, m))
-            d = np.zeros((n))
-            initial_schedule = np.zeros(n, dtype=np.int32)
-            for i in range(0, n):
-                initial_schedule[i] = i
-                for j in range(0, m):
-                    p[i][j] = generator.nextInt(1, 99)
-                    A += p[i][j]
-            B = A//2
-            A = A//6
-            for i in range(0, n):
-                d[j] = generator.nextInt(A, B)
-            # Losowanie początkowego harmonogramu
-            # np.random.shuffle(initial_schedule)
-            print(all_task_done_time(
-                initial_schedule, n, m, p, d, [2, 3, 4, 5]))
-            print(p)
-            print(initial_schedule)
+        A = 0
+        # Generacja instancji
+        p = np.zeros((n, m))
+        d = np.zeros((n))
+        initial_schedule = np.zeros(n, dtype=np.int32)
+        for i in range(0, n):
+            initial_schedule[i] = i
+            for j in range(0, m):
+                p[i][j] = generator.nextInt(1, 99)
+                A += p[i][j]
+        B = A//2
+        A = A//6
+        for i in range(0, n):
+            d[j] = generator.nextInt(A, B)
+
+        max_iterations = 10
+        for max_iterations in [100, 200, 400, 800, 1600]:
+            P = []
+            iters = 0
+            random.shuffle(initial_schedule)
+            x = initial_schedule
+            P.append(all_task_done_time(x, n, m, p, d, [2, 3]))
+            while iters < max_iterations:
+                j = generator.nextInt(0, len(x)-1)
+                i = generator.nextInt(0, len(x)-1)
+                while j == i:
+                    i = generator.nextInt(0, len(x)-1)
+                x_prim = x.copy()
+                x_prim[i], x_prim[j] = x_prim[j], x_prim[i]
+                if(is_dominated(all_task_done_time(x_prim, n, m, p, d, [2, 3]), all_task_done_time(x, n, m, p, d, [2, 3])) or random.random() < pow(0.995, iters)):
+                    P.append(all_task_done_time(x_prim, n, m, p, d, [2, 3]))
+                x = x_prim
+                iters += 1
+            F = make_Pareto_from_P(P)
+            F = np.array(F)
+            print(F[:, 0])
+            plt.plot(F[:, 1], F[:, 0], 'o')
+            plt.show()
 
 
-do_experiments(1, 3, 3)
+do_zadanie1(10, 10)
